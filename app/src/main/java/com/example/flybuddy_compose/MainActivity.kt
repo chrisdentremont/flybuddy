361295b9e3573a2f22ac55b02733f03d2e38302f
dentremontcatwit <dentremontc@wit.edu>
2023-04-04 19:53:57 -0400

    Added fonts and base composable functions

--- app/src/main/java/com/example/flybuddy_compose/MainActivity.kt
+++ app/src/main/java/com/example/flybuddy_compose/MainActivity.kt
@@ -9,14 +9,20 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavDestination.Companion.hierarchy
 import androidx.navigation.NavGraph.Companion.findStartDestination
 import androidx.navigation.compose.NavHost
@@ -46,14 +52,17 @@ class MainActivity : ComponentActivity() {
 sealed class Screen(val route: String, @StringRes val resourceId: Int) {
     object Home : Screen("home", R.string.home_name)
     object Search : Screen("flightSearch", R.string.search_name)
+    object Friends : Screen("friends", R.string.friends_name)
     object Settings : Screen("settings", R.string.settings_name)
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun NavBar(){
     val routeMap = mapOf(
         Screen.Home to Icons.Filled.Home,
+        Screen.Friends to Icons.Filled.Person,
         Screen.Search to Icons.Filled.Search,
         Screen.Settings to Icons.Filled.Settings
     )
@@ -93,6 +102,7 @@ fun NavBar(){
         innerPadding ->
         NavHost(navController, startDestination = Screen.Home.route, Modifier.padding(innerPadding)) {
             composable(Screen.Home.route) {Home()}
+            composable(Screen.Friends.route) {Friends()}
             composable(Screen.Search.route) { FlightSearch() }
             composable(Screen.Settings.route) { Settings() }
         }
@@ -102,14 +112,33 @@ fun NavBar(){
 
 }
 
+val poppinsFamily = FontFamily(
+    Font(R.font.poppins_regular, FontWeight.Normal),
+    Font(R.font.poppins_bold, FontWeight.Bold),
+    Font(R.font.poppins_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.poppins_light, FontWeight.Light),
+)
+
+val nunitoFamily = FontFamily(
+    Font(R.font.nunito_regular, FontWeight.Normal),
+    Font(R.font.nunito_bold, FontWeight.Bold),
+    Font(R.font.nunito_italic, FontWeight.Normal, FontStyle.Italic),
+    Font(R.font.nunito_light, FontWeight.Light)
+)
+
 @Composable
 fun Home(){
-    Card(
-        elevation = 10.dp,
+    Text(
         modifier = Modifier.padding(24.dp),
-    ){
-        Text("Card Content")
-    }
+        fontSize = 30.sp,
+        fontFamily = poppinsFamily,
+        fontWeight = FontWeight.Normal,
+        text = "Card Content")
+}
+
+@Composable
+fun Friends(){
+    Text(text = "This is the friends page")
 }
 
 @Composable
