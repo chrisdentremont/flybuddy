dbfc578e8693358220707668a1cbec37507f3969
dentremontcatwit <dentremontc@wit.edu>
2023-04-06 19:06:02 -0400

    Added info to flight view window

--- app/src/main/java/com/example/flybuddy_compose/HomePage.kt
+++ app/src/main/java/com/example/flybuddy_compose/HomePage.kt
@@ -105,12 +105,47 @@ fun HomeCards(){
                 showFlightBox = false
             },
             title = {
-                var flightName = selectedFlight.flight.number
-                Text("Flight $flightName")
+                Column(modifier = Modifier.padding(bottom = 5.dp)){
+                    var flightName = if (selectedFlight.flight.number == null) "N/A" else selectedFlight.flight.number
+                    var flightDate = selectedFlight.flight_date
+                    Text("Flight $flightName", fontSize = 30.sp, fontFamily = poppinsFamily, fontWeight = FontWeight.Normal)
+                    Text("$flightDate", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                }
             },
             text = {
-                var flightAirline = selectedFlight.airline.name
-                Text("Airline: $flightAirline")
+                Column(){
+                    var flightAirline = if (selectedFlight.airline.name == null) "N/A" else selectedFlight.airline.name
+                    var flightAirlineIata = if (selectedFlight.airline.iata == null) "N/A" else selectedFlight.airline.iata
+                    var flightAirlineIcao = if (selectedFlight.airline.icao == null) "N/A" else selectedFlight.airline.icao
+                    Text("Airline", fontSize = 20.sp, fontFamily = poppinsFamily, fontWeight = FontWeight.Normal)
+                    Text("Name: $flightAirline", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("IATA Code: $flightAirlineIata", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("ICAO Code: $flightAirlineIcao", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+
+                    Divider(modifier = Modifier.padding(top = 5.dp, bottom = 5.dp))
+
+                    var departureAirport = if (selectedFlight.departure.airport == null) "N/A" else selectedFlight.departure.airport
+                    var departureEstimated = if (selectedFlight.departure.estimated == null) "N/A" else selectedFlight.departure.estimated
+                    var departureTerminal = if (selectedFlight.departure.terminal == null) "N/A" else selectedFlight.departure.terminal
+                    var departureZone = if (selectedFlight.departure.timezone == null) "N/A" else selectedFlight.departure.timezone
+                    Text("Departure", fontSize = 20.sp, fontFamily = poppinsFamily, fontWeight = FontWeight.Normal)
+                    Text("Airport: $departureAirport", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Estimated Departure Time: $departureEstimated", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Terminal: $departureTerminal", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Time Zone: $departureZone", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+
+                    Divider(modifier = Modifier.padding(top = 5.dp, bottom = 5.dp))
+
+                    var arrivalAirport = if (selectedFlight.arrival.airport == null) "N/A" else selectedFlight.arrival.airport
+                    var arrivalEstimated = if (selectedFlight.arrival.estimated == null) "N/A" else selectedFlight.arrival.estimated
+                    var arrivalTerminal = if (selectedFlight.arrival.terminal == null) "N/A" else selectedFlight.arrival.terminal
+                    var arrivalZone = if (selectedFlight.arrival.timezone == null) "N/A" else selectedFlight.arrival.timezone
+                    Text("Arrival", fontSize = 20.sp, fontFamily = poppinsFamily, fontWeight = FontWeight.Normal)
+                    Text("Airport: $arrivalAirport", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Estimated Arrival Time: $arrivalEstimated", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Terminal: $arrivalTerminal", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                    Text("Time Zone: $arrivalZone", fontSize = 15.sp, fontFamily = nunitoFamily, fontWeight = FontWeight.Normal)
+                }
             },
             confirmButton = {
                 Button(
